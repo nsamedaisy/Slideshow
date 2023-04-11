dbf18f6d252190b49feb6ff6facef2db4d500a78
nsamedaisy <nsamedaisy@gmail.com>
2023-04-11 14:35:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// let img = document.getElementsByClassName("slides");
+// let imgArr = [
+//     "assets/images/car.jpg"
+//     "assets/images/car.jpg"
+//     "assets/images/car.jpg"
+//     "assets/images/car.jpg"
+//     "assets/images/car.jpg"
+//     "assets/images/car.jpg"
+// ]
