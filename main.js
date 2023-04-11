2c5677d891f28720d752b5b8514758bed75c9ce3
nsamedaisy <nsamedaisy@gmail.com>
2023-04-11 18:09:50 +0100

    my slideshow uncomlete

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-// let img = document.getElementsByClassName("slides");
-// let imgArr = [
-//     "assets/images/car.jpg"
-//     "assets/images/car.jpg"
-//     "assets/images/car.jpg"
-//     "assets/images/car.jpg"
-//     "assets/images/car.jpg"
-//     "assets/images/car.jpg"
-// ]
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
